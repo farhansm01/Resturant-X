825119a150b97a487ce0416c4deefea3b81f5f1f
Iftekhar Ferdous <mohammodiftee@gmail.com>
2025-03-09 04:10:19 +0600

    main class

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import GUI.*;
+import GUI.DashboardPage;
+//import File.*;
+//package GUI;
+
+public class Main{
+	public static void main(String []args){
+		//Res bmp = new Res();
+		//Regis bnp = new Regis();
+		//ReservationGUI bpp = new ReservationGUI();
+		Login bgp = new Login();
+		
+		//DashboardPage a = new DashboardPage();
+		//MenuGui b = new MenuGui(); 
+	}
+}
